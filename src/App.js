817b778787bb1714983979b94358adc576b6da2c
Cansur <tjswo0682@gmail.com>
2024-12-31 20:03:25 +0900

    spring boot와 연동

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  
+  // spring boot 연결
+  const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
   return (
     <div>
-      초기 설정
+      백엔드에서 가져온 데이터입니다 : {hello}
     </div>
   );
 }
