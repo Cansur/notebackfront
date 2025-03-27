cd5efce03b4c612d719e623011e5ae15d9b5ff0f
Cansur <tjswo0682@gmail.com>
2025-03-27 23:00:49 +0900

    Feature: blockNotejs 기능 추가
    Feature: blockNotejs를 사용하여 간단한  localStroge 저장, 로드 기능 추가

--- src/components/MDEditorComp.js
+++ src/components/MDEditorComp.js
@@ -1,40 +1,53 @@
-// src/components/MDEditorComp.jsx
-import React, { useState, useEffect } from "react";
-import MDEditor from "@uiw/react-md-editor";
-import axiosInstance from "../api/axiosInstance";
+import { useEffect, useState, useMemo } from "react";
 
-const MDEditorComp = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [viewMode, setViewMode] = useState("edit"); // 🔹 초기값을 'edit'으로 설정
+import { Block, BlockNoteEditor } from "@blocknote/core";
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
+
+// localStrorage에 저장
+async function saveToStorage(jsonBlocks: Block[]) {
+  localStorage.setItem("blocks", JSON.stringify(jsonBlocks));
+}
+
+// localStorage에서 불러오기
+async function loadFromStorage() {
+  const blocks = localStorage.getItem("blocks");
+  if (blocks) {
+    return JSON.parse(blocks);
+  }
+  return [];
+}
+
+export default function MDEditorComp() {
+  const [blocks, setBlocks] = useState([]);
+  const [initialContent, setInitialContent] = useState();
+
+  // const editor = useCreateBlockNote();
+  const editor = useMemo(() => {
+    return BlockNoteEditor.create({ initialContent });
+  }, [initialContent])
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const lastBoardId = localStorage.getItem("lastBoardId");
-        if (!lastBoardId) return;
-        const response = await axiosInstance.get(`/board/${lastBoardId}`);
-        setTitle(response.data.title);
-        setContent(response.data.content);
-      } catch (error) {
-        console.error("에러 발생:", error);
-      }
-    };
-
-    getData();
+    loadFromStorage().then((blocks) => {
+      // setBlocks(blocks);
+      setInitialContent(blocks);
+    });
   }, []);
 
+  useEffect(() => {
+    console.log(blocks);
+  }, [blocks]);
+
   return (
-    <div className="w-full bg-gray-900 text-white p-6 rounded-lg shadow-md">
-      <input
-        className="w-full bg-transparent text-3xl border-b-2 border-gray-500 focus:border-blue-400 outline-none p-2"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="제목을 입력하세요"
-      />
-      <MDEditor height={650} value={content} onChange={setContent} visibleDragbar={false} preview={viewMode} />
+    <div>
+      <BlockNoteView
+        editor={editor}
+        onChange={() => {
+          // setBlocks(editor.document);
+          saveToStorage(editor.document);
+        }} />
     </div>
   );
-};
-
-export default MDEditorComp;
+}
